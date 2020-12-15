3a3f9b15d3c9f80b1bc5592c46ae55cae083eca2
Alexandr Berbat <42623575+AlexandrBerbat@users.noreply.github.com>
2020-12-15 23:50:46 +0200

    Update Source.cpp

--- lab1OS/lab1OS/Source.cpp
+++ lab1OS/lab1OS/Source.cpp
@@ -5,17 +5,17 @@
 #include <malloc.h>
 #include <assert.h>
 
-#define HEAP_SIZE 1024
+#define HEAP_SIZE 4096
 
 static char heap[HEAP_SIZE];
 static void* PTR;
 
-// size_t беззнаковый тип, используемый для подсчета или индексации массива
-static size_t SIZE = 812;
-#pragma pack(push, 1) //заносит текущее значение на вершину
+
+static size_t SIZE = 2501;
+#pragma pack(push, 1) 
 typedef struct header
 {
-	bool status; // 0 - свободно, 1 - занято
+	bool status; 
 	size_t previous_size;
 	size_t size;
 }header_t;
@@ -47,7 +47,7 @@ size_t get_size(void* pointer)
 }
 void* get_next(void* pointer)
 {
-	// uint8_t - беззнаковое целое размером 8 бит
+	
 	if ((uint8_t*)pointer + get_size(pointer) + SIZE_H == (uint8_t*)PTR + SIZE + SIZE_H)
 	{
 		return NULL;
@@ -70,11 +70,11 @@ void new_header(void* pointer, bool status, size_t previous_size, size_t size)
 	a.size = size;
 	*((header_t*)pointer) = a;
 }
-void combine_headers(void* pointer1, void* pointer2) // сделать 1 блок из 2
+void combine_headers(void* pointer1, void* pointer2) 
 {
 	assert(get_status(pointer1) == 0 && get_status(pointer2) == 0);
 	set_size(pointer1, get_size(pointer1) + get_size(pointer2) + SIZE_H);
-	// Если следующий указатель существует, обновите его информацию о предыдущем блоке
+	
 	if (get_next(pointer2) != NULL)
 	{
 		set_previous_size(get_next(pointer2), get_size(pointer1));
@@ -82,45 +82,40 @@ void combine_headers(void* pointer1, void* pointer2) // сделать 1 блок из 2
 }
 void* block(size_t size)
 {
-	//void* pointer = malloc(size + SIZE_H);
-	//size -= SIZE_H;
+	
 	void* pointer = heap + SIZE_H;
 	new_header(pointer, false, 0, size);
 
 	return pointer;
 }
-void* get_best(size_t size) //выбераем лучшую область памяти
+void* get_best(size_t size)
 {
 	void* pointer = PTR;
 	void* best = NULL;
 	while (pointer != NULL)
 	{
-		if ((best == NULL || get_size(best) > get_size(pointer)) && get_size(pointer) // если лучшее пусто или в нем больше пустого места
-			>= size && get_status(pointer) == 0) // и указатель не занят
+		if ((best == NULL || get_size(best) > get_size(pointer)) && get_size(pointer)
+			>= size && get_status(pointer) == 0) 
 		{
 			best = pointer;
 		}
-		pointer = get_next(pointer); // перейти к следующему 
+		pointer = get_next(pointer);
 	}
 	return best;
 }
 
 void* mem_alloc(size_t size)
 {
-	if (size % 4 != 0) //выровнять 4 байта
+	if (size % 4 != 0)
 	{
 		size = size - size % 4 + 4;
 	}
 	void* pointer = get_best(size);
 	if (pointer == NULL)
 	{
-		return pointer; // не может выделить, так как нет пустой области
+		return pointer;
 	}
-	//assert(pointer != NULL);
 
-	// если размер найденного блока больше требуемого
-	// создать новый заголовок в конце нового блока, который будет работать до конца блока
-	// (разделение большого блока на два: один выделяется, другой - нет)
 	if (get_size(pointer) > size + SIZE_H)
 	{
 		new_header((uint8_t*)pointer + size + SIZE_H, 0, size, get_size(pointer) - size - SIZE_H);
@@ -133,11 +128,11 @@ void mem_free(void* pointer)
 {
 	pointer = (uint8_t*)pointer - SIZE_H;
 	set_status(pointer, false);
-	// если следующий свободен, объедините два блока
+	
 	if (get_next(pointer) != NULL && get_status(get_next(pointer)) == 0)
 	{
 		combine_headers(pointer, get_next(pointer));
-	} // если предыдущее пусто, объединить текущий с предыдущим
+	} 
 	if (get_previous(pointer) != NULL && get_status(get_previous(pointer)) == 0)
 	{
 		combine_headers(get_previous(pointer), pointer);
@@ -154,9 +149,9 @@ void* mem_realloc(void* pointer, size_t size)
 	{
 		return pointer;
 	}
-	if (get_size(pointer) > size) // если в существующем блоке больше памяти, чем необходимо
+	if (get_size(pointer) > size) 
 	{
-		if (get_size(pointer) - size - SIZE_H >= 0) // новый размер должен быть больше, чем размер заголовка (достаточно места для формирования нового блока)
+		if (get_size(pointer) - size - SIZE_H >= 0)
 		{
 			new_header((uint8_t*)pointer + size + SIZE_H, false, size, get_size(pointer) - size -
 				SIZE_H);
@@ -170,21 +165,21 @@ void* mem_realloc(void* pointer, size_t size)
 		return pointer;
 
 	}
-	if (get_next(pointer) != NULL && get_size(pointer) + get_size(get_next(pointer)) //если есть следующий блок size_1 + size_2> = size
+	if (get_next(pointer) != NULL && get_size(pointer) + get_size(get_next(pointer))
 		>= size)
 	{
 		new_header((uint8_t*)pointer + size + SIZE_H, false, size, get_size(get_next(pointer)) -
-			(size - get_size(pointer))); //создайте новый блок с начала первого блока, первый блок теперь между новым и вторым
+			(size - get_size(pointer)));
 		set_size(pointer, size);
 		return pointer;
 	}
 	if (get_next(pointer) != NULL && get_status(get_next(pointer)) == 0 &&
-		get_size(pointer) + get_size(get_next(pointer)) + SIZE_H >= size) // если второй блок свободен, просто добавьте его во второй блок
+		get_size(pointer) + get_size(get_next(pointer)) + SIZE_H >= size)
 	{
 		set_size(pointer, get_size(pointer) + get_size(get_next(pointer)) + SIZE_H);
 		return pointer;
 	}
-	void* best = mem_alloc(size); //если новый размер больше, чем блок1 + блок2
+	void* best = mem_alloc(size);
 	if (best == NULL)
 	{
 		return best;
